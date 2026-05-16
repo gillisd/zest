a8f999079aece8452b65ad1a2e42151e7b879c2a
David Gillis <david@flipmine.com>
2026-05-16 20:59:22 -0300

    Add symmetric refute coverage for true/false assertions

--- test/assertions/test_assert_true_false.zsh
+++ test/assertions/test_assert_true_false.zsh
@@ -28,8 +28,23 @@ test_refute_true_with_0() {
   assert_equal 0 $rc
 }
 
+test_refute_true_with_false() {
+  run refute_true false
+  assert_equal 0 $rc
+}
+
 test_refute_true_with_1() {
   run refute_true 1
+  assert_equal 1 $rc
+}
+
+test_refute_true_with_true() {
+  run refute_true true
+  assert_equal 1 $rc
+}
+
+test_refute_true_with_nonempty_string() {
+  run refute_true anything
   assert_equal 1 $rc
 }
 
@@ -53,12 +68,32 @@ test_assert_false_with_true() {
   assert_equal 1 $rc
 }
 
+test_assert_false_with_nonempty_string() {
+  run assert_false anything
+  assert_equal 1 $rc
+}
+
 test_refute_false_with_1() {
   run refute_false 1
   assert_equal 0 $rc
 }
 
+test_refute_false_with_true() {
+  run refute_false true
+  assert_equal 0 $rc
+}
+
+test_refute_false_with_nonempty_string() {
+  run refute_false anything
+  assert_equal 0 $rc
+}
+
 test_refute_false_with_0() {
   run refute_false 0
+  assert_equal 1 $rc
+}
+
+test_refute_false_with_false() {
+  run refute_false false
   assert_equal 1 $rc
 }
