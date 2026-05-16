c8b870eaa3fa608c8bebc3a5dc5f945eb7745bbc
David Gillis <david@flipmine.com>
2026-05-16 20:59:47 -0300

    Add symmetric refute coverage for comparison assertions

--- test/assertions/test_assert_comparison.zsh
+++ test/assertions/test_assert_comparison.zsh
@@ -23,6 +23,16 @@ test_refute_greater_than_on_success() {
   assert_equal 0 $rc
 }
 
+test_refute_greater_than_equal_values() {
+  run refute_greater_than 5 5
+  assert_equal 0 $rc
+}
+
+test_refute_greater_than_negative() {
+  run refute_greater_than 0 -10
+  assert_equal 0 $rc
+}
+
 test_refute_greater_than_on_failure() {
   run refute_greater_than 5 10
   assert_equal 1 $rc
@@ -43,8 +53,23 @@ test_assert_less_than_equal_values() {
   assert_equal 1 $rc
 }
 
+test_assert_less_than_negative() {
+  run assert_less_than 0 -10
+  assert_equal 0 $rc
+}
+
 test_refute_less_than_on_success() {
   run refute_less_than 5 10
+  assert_equal 0 $rc
+}
+
+test_refute_less_than_equal_values() {
+  run refute_less_than 5 5
+  assert_equal 0 $rc
+}
+
+test_refute_less_than_negative() {
+  run refute_less_than -10 0
   assert_equal 0 $rc
 }
 
