0081cd8d947c045cebdf342472961ad3a747502a
David Gillis <david@flipmine.com>
2026-05-16 20:54:41 -0300

    Add tests for assert_greater_than, assert_less_than, and refute variants

---
+++ test/assertions/test_assert_comparison.zsh
@@ -0,0 +1,54 @@
+test_assert_greater_than_on_success() {
+  run assert_greater_than 5 10
+  assert_equal 0 $rc
+}
+
+test_assert_greater_than_on_failure() {
+  run assert_greater_than 10 5
+  assert_equal 1 $rc
+}
+
+test_assert_greater_than_equal_values() {
+  run assert_greater_than 5 5
+  assert_equal 1 $rc
+}
+
+test_assert_greater_than_negative() {
+  run assert_greater_than -10 0
+  assert_equal 0 $rc
+}
+
+test_refute_greater_than_on_success() {
+  run refute_greater_than 10 5
+  assert_equal 0 $rc
+}
+
+test_refute_greater_than_on_failure() {
+  run refute_greater_than 5 10
+  assert_equal 1 $rc
+}
+
+test_assert_less_than_on_success() {
+  run assert_less_than 10 5
+  assert_equal 0 $rc
+}
+
+test_assert_less_than_on_failure() {
+  run assert_less_than 5 10
+  assert_equal 1 $rc
+}
+
+test_assert_less_than_equal_values() {
+  run assert_less_than 5 5
+  assert_equal 1 $rc
+}
+
+test_refute_less_than_on_success() {
+  run refute_less_than 5 10
+  assert_equal 0 $rc
+}
+
+test_refute_less_than_on_failure() {
+  run refute_less_than 10 5
+  assert_equal 1 $rc
+}
