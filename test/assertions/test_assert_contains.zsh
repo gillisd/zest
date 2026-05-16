55656e8cf00d1f53c34edebda15e70e1fe1187ed
David Gillis <david@flipmine.com>
2026-05-16 16:26:42 -0300

    Add tests for assert_contains

---
+++ test/assertions/test_assert_contains.zsh
@@ -0,0 +1,59 @@
+test_assert_contains_string_on_success() {
+  run assert_contains "hello world" "world"
+  assert_equal 0 $rc
+}
+
+test_assert_contains_string_on_failure() {
+  run assert_contains "hello world" "mars"
+  assert_equal 1 $rc
+}
+
+test_assert_contains_string_exact_match() {
+  run assert_contains "foo" "foo"
+  assert_equal 0 $rc
+}
+
+test_assert_contains_string_empty_needle() {
+  run assert_contains "foo" ""
+  assert_equal 0 $rc
+}
+
+test_assert_contains_array_ref_on_success() {
+  local -a fruit=(apple cherry banana)
+  run assert_contains fruit cherry
+  assert_equal 0 $rc
+}
+
+test_assert_contains_array_ref_on_failure() {
+  local -a fruit=(apple cherry banana)
+  run assert_contains fruit mango
+  assert_equal 1 $rc
+}
+
+test_assert_contains_array_ref_empty_array() {
+  local -a fruit=()
+  run assert_contains fruit apple
+  assert_equal 1 $rc
+}
+
+test_refute_contains_string_on_success() {
+  run refute_contains "hello world" "mars"
+  assert_equal 0 $rc
+}
+
+test_refute_contains_string_on_failure() {
+  run refute_contains "hello world" "world"
+  assert_equal 1 $rc
+}
+
+test_refute_contains_array_ref_on_success() {
+  local -a fruit=(apple cherry banana)
+  run refute_contains fruit mango
+  assert_equal 0 $rc
+}
+
+test_refute_contains_array_ref_on_failure() {
+  local -a fruit=(apple cherry banana)
+  run refute_contains fruit cherry
+  assert_equal 1 $rc
+}
