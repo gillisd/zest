209f8ecd15df6729f0881d0d35e5efac0715fce2
David Gillis <david@flipmine.com>
2026-05-16 16:25:51 -0300

    Preserve empty args in run

--- test/zest_run_test.zsh
+++ test/zest_run_test.zsh
@@ -7,3 +7,18 @@ test_run_err() {
   run print -u2 foo
   assert_equal foo $err
 }
+
+test_run_rc_on_success() {
+  run true
+  assert_equal 0 $rc
+}
+
+test_run_rc_on_failure() {
+  run false
+  assert_equal 1 $rc
+}
+
+test_run_preserves_empty_args() {
+  run assert_contains "" "foo"
+  assert_equal 1 $rc
+}
