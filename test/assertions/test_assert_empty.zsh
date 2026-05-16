2ba9d28a403e0baa8a2a78db8411eaf604360757
David Gillis <david@flipmine.com>
2026-05-16 15:11:22 -0300

    Add preliminary tests for assert_empty

---
+++ test/assertions/test_assert_empty.zsh
@@ -0,0 +1,47 @@
+test_assert_empty_array_on_success() {
+  local -a fruit=()
+  run assert_empty fruit
+  assert_equal 0 $rc
+}
+
+test_assert_empty_array_on_error() {
+  local -a fruit=(cherry)
+  run assert_empty fruit
+  assert_equal 1 $rc
+}
+
+test_refute_empty_array_on_success() {
+  local -a fruit=(cherry)
+  run refute_empty fruit
+  assert_equal 0 $rc
+}
+
+test_refute_empty_array_on_error() {
+  local -a fruit=()
+  run refute_empty fruit
+  assert_equal 1 $rc
+}
+
+test_assert_empty_string_on_success() {
+  local string=""
+  run assert_empty $string
+  assert_equal 0 $rc
+}
+
+test_assert_empty_string_on_error() {
+  local string="foo"
+  run assert_empty $string
+  assert_equal 1 $rc
+}
+
+test_refute_empty_string_on_success() {
+  local string="foo"
+  run refute_empty $string
+  assert_equal 0 $rc
+}
+
+test_refute_empty_string_on_error() {
+  local string=""
+  run refute_empty $string
+  assert_equal 1 $rc
+}
