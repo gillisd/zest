eb793f767acc218851adc4766da74e01e414f6da
David Gillis <david@flipmine.com>
2026-05-16 16:33:18 -0300

    Add tests for assert_match and refute_match

---
+++ test/assertions/test_assert_match.zsh
@@ -0,0 +1,34 @@
+test_assert_match_on_success() {
+  run assert_match "hello world" "^hello"
+  assert_equal 0 $rc
+}
+
+test_assert_match_on_failure() {
+  run assert_match "hello world" "^world"
+  assert_equal 1 $rc
+}
+
+test_assert_match_full_line() {
+  run assert_match "abc123" "^[a-z]+[0-9]+$"
+  assert_equal 0 $rc
+}
+
+test_assert_match_partial() {
+  run assert_match "foo-bar-baz" "bar"
+  assert_equal 0 $rc
+}
+
+test_assert_match_special_chars() {
+  run assert_match "file.txt" "\.txt$"
+  assert_equal 0 $rc
+}
+
+test_refute_match_on_success() {
+  run refute_match "hello world" "^world"
+  assert_equal 0 $rc
+}
+
+test_refute_match_on_failure() {
+  run refute_match "hello world" "^hello"
+  assert_equal 1 $rc
+}
