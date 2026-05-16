e7a8123f3a0c993d34362b26f1aae4da6ec7d242
David Gillis <david@flipmine.com>
2026-05-16 16:38:43 -0300

    Fix skip tests to verify behavior as PASS instead of just showing SKIP

--- test/test_skip.zsh
+++ test/test_skip.zsh
@@ -1,9 +1,28 @@
-test_skip_marks_test_as_skipped() {
+test_skip_sets_skipped_flag() {
+  zest_current_skipped=false
   skip
-  assert_equal "this should not run" "but it does"
+  local was_skipped=$zest_current_skipped
+  zest_current_skipped=false
+  assert_equal true $was_skipped
 }
 
-test_skip_with_message() {
-  skip "not implemented yet"
-  assert_equal "this should not run" "but it does"
+test_skip_sets_message() {
+  zest_current_skipped=false
+  zest_current_skip_msg=""
+  skip "not ready"
+  local was_skipped=$zest_current_skipped
+  local msg=$zest_current_skip_msg
+  zest_current_skipped=false
+  zest_current_skip_msg=""
+  assert_equal true $was_skipped
+  assert_equal "not ready" $msg
+}
+
+test_skip_without_message_leaves_empty_msg() {
+  zest_current_skip_msg="stale"
+  skip
+  local msg=$zest_current_skip_msg
+  zest_current_skipped=false
+  zest_current_skip_msg=""
+  assert_empty $msg
 }
