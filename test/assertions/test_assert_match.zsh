81e2cf8cda9eaccff87347a3ddd15c88f8f82e82
David Gillis <david@flipmine.com>
2026-05-16 21:09:59 -0300

    Add refute_match tests for full line, partial, and special chars

--- test/assertions/test_assert_match.zsh
+++ test/assertions/test_assert_match.zsh
@@ -32,3 +32,18 @@ test_refute_match_on_failure() {
   run refute_match "hello world" "^hello"
   assert_equal 1 $rc
 }
+
+test_refute_match_full_line() {
+  run refute_match "abc123" "^[a-z]+[0-9]+$"
+  assert_equal 1 $rc
+}
+
+test_refute_match_partial() {
+  run refute_match "foo-bar-baz" "bar"
+  assert_equal 1 $rc
+}
+
+test_refute_match_special_chars() {
+  run refute_match "file.txt" "\.txt$"
+  assert_equal 1 $rc
+}
