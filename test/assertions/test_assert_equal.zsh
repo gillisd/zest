430f34b6e95d4161734e6be653c20bd074d7e6e6
David Gillis <david@flipmine.com>
2026-05-16 21:09:05 -0300

    Add refute_equal tests for spaces, numerics, and newline edge cases

--- test/assertions/test_assert_equal.zsh
+++ test/assertions/test_assert_equal.zsh
@@ -58,6 +58,56 @@ test_refute_equal_empty_vs_nonempty() {
   assert_equal 0 $rc
 }
 
+test_refute_equal_with_spaces() {
+  run refute_equal "hello world" "hello  world"
+  assert_equal 0 $rc
+}
+
+test_refute_equal_with_spaces_match() {
+  run refute_equal "hello world" "hello world"
+  assert_equal 1 $rc
+}
+
+test_refute_equal_numeric_strings() {
+  run refute_equal 42 043
+  assert_equal 0 $rc
+}
+
+test_refute_equal_numeric_strings_match() {
+  run refute_equal 42 42
+  assert_equal 1 $rc
+}
+
+test_refute_equal_trailing_newline_mismatch() {
+  run refute_equal $'foo\n' "foo"
+  assert_equal 0 $rc
+}
+
+test_refute_equal_trailing_newline_match() {
+  run refute_equal $'foo\n' $'foo\n'
+  assert_equal 1 $rc
+}
+
+test_refute_equal_embedded_newline_mismatch() {
+  run refute_equal $'foo\nbar' $'foobar'
+  assert_equal 0 $rc
+}
+
+test_refute_equal_embedded_newlines_match() {
+  run refute_equal $'foo\nbar' $'foo\nbar'
+  assert_equal 1 $rc
+}
+
+test_refute_equal_crlf_vs_lf() {
+  run refute_equal $'foo\r\n' $'foo\n'
+  assert_equal 0 $rc
+}
+
+test_refute_equal_newline_only_vs_empty() {
+  run refute_equal $'\n' ""
+  assert_equal 0 $rc
+}
+
 test_assert_equal_trailing_newline_mismatch() {
   run assert_equal $'foo\n' "foo"
   assert_equal 1 $rc
